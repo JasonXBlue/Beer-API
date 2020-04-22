295c472744fdfd8c52aad3691ffb958336b0f2ac
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-21 19:42:25 -0500

    table works

--- Beer/src/app/app.module.ts
+++ Beer/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BeerComponent } from './beer/beer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { BeerTableComponent } from './beer-table/beer-table.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, BeerComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, BeerComponent, BeerTableComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ Beer/src/app/beer-table/beer-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { ApiService } from '../services/api.service';
+import { IBeer } from '../interfaces/ibeer';
+
+@Component({
+  selector: 'app-beer-table',
+  templateUrl: './beer-table.component.html',
+  styleUrls: ['./beer-table.component.css'],
+})
+export class BeerTableComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'tagline'];
+
+  dataSource = new MatTableDataSource<IBeer>();
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(private apiService: ApiService) {}
+
+  async ngOnInit() {
+    this.dataSource = new MatTableDataSource(await this.apiService.get());
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(filter: string): void {
+    this.dataSource.filter = filter.trim().toLowerCase();
+  }
+}
